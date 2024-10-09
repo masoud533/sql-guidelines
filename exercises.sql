db220f8e4f67bf024acb77aa04d43957bf52fc12
masoud533 <mr.masoud_mapar@outlook.com>
2024-10-09 15:21:30 +0330

    add exeecises file for practice sql syntax

---
+++ exercises.sql
@@ -0,0 +1,15 @@
+SELECT *
+FROM customers
+WHERE first_name REGEXP 'ELKA|AMBUR';
+
+SELECT *
+FROM customers
+WHERE last_name REGEXP 'EY$|ON$';
+
+SELECT *
+FROM customers
+WHERE last_name REGEXP '^MY|SE';
+
+SELECT *
+FROM customers
+WHERE last_name REGEXP 'B[RU]';
