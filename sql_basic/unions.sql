c0f012c7fc6ead7cc93855d37e08fa2d4ccf410a
masoud533 <mr.masoud_mapar@outlook.com>
2024-10-15 21:34:45 +0330

    modified unions.sql

--- sql_basic/unions.sql
+++ sql_basic/unions.sql
@@ -20,3 +20,28 @@ from shippers;
 
 -- > warning : the number of columns that the etch query returns should be equal
   -- otherwise you're going to get an error
+
+select
+    customer_id,
+    first_name,
+    points,
+    'bronze' as type
+from customers
+where points < 2000
+union
+select
+    customer_id,
+    first_name,
+    points,
+    'silver' as type
+from customers
+where points between 2000 and 3000
+union
+select
+    customer_id,
+    first_name,
+    points,
+    'gold' as type
+from customers
+where points > 3000
+order by first_name;
